af768dca4eeed779a75242592d1a726834f67641
Francesco Placidi <francesco.placidi@linksmt.it>
2022-06-24 16:36:43 +0200

    Migration in 2 steps

---
+++ src/main/resources/db/migration/V1.0.0__CREATE_movies.sql
@@ -0,0 +1,5 @@
+CREATE TABLE public.movies (
+	id serial8 NOT NULL,
+	title text NOT NULL,
+	CONSTRAINT movies_pkey PRIMARY KEY (id)
+);
--- src/main/resources/db/migration/V1.1.0__INSERT_movies.sql
+++ src/main/resources/db/migration/V1.1.0__INSERT_movies.sql
@@ -1,5 +1,3 @@
-DROP TABLE IF EXISTS movies;
-CREATE TABLE movies (id SERIAL PRIMARY KEY, title TEXT NOT NULL);
 INSERT INTO movies (title) VALUES('The Lord of the Rings');
 INSERT INTO movies (title) VALUES('Harry Potter');
 INSERT INTO movies (title) VALUES('Soul');
