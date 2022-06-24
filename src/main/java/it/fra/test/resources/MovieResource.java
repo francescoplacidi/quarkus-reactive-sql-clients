946c70d173e4aa1d00a0acb6f3efe028ff15f14f
Francesco Placidi <francesco.placidi@linksmt.it>
2022-06-24 09:35:43 +0200

    Other CRUD methods

--- src/main/java/it/fra/test/resources/MovieResource.java
+++ src/main/java/it/fra/test/resources/MovieResource.java
@@ -1,15 +1,24 @@
 package it.fra.test.resources;
 
-import io.smallrye.common.annotation.Blocking;
 import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
 import io.vertx.mutiny.pgclient.PgPool;
 import it.fra.test.models.Movie;
 import lombok.extern.slf4j.Slf4j;
 
+import java.net.URI;
+import java.util.Objects;
+
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+import static javax.ws.rs.core.Response.Status;
 
 @Slf4j
 @Path("movies")
@@ -26,6 +35,35 @@ public class MovieResource {
     @GET
     public Multi<Movie> getAll() {
         return Movie.findAll(client);
+    }
+
+    @GET
+    @Path("{id}")
+    public Uni<Response> getById(@PathParam("id") Long id) {
+        return Movie.findById(client, id)
+            .onItem()
+            .transform(movie -> Objects.nonNull(movie) ? Response.ok(movie) : Response.status(Status.NOT_FOUND))
+            .onItem()
+            .transform(Response.ResponseBuilder::build);
+    }
+
+    @POST
+    public Uni<Response> create(Movie movie) {
+        return Movie.save(client, movie)
+            .onItem()
+            .transform(id -> URI.create(String.format("movies/%s", id)))
+            .onItem()
+            .transform(uri -> Response.created(uri).build());
+    }
+
+    @DELETE
+    @Path("{id}")
+    public Uni<Response> delete(@PathParam("id") Long id) {
+        return Movie.delete(client, id)
+            .onItem()
+            .transform(deleted -> deleted.booleanValue() ? Status.NO_CONTENT : Status.NOT_FOUND)
+            .onItem()
+            .transform(status -> Response.status(status).build());
     }
 
     private void initDb() {
